931232ca7cfbcdce9b0a19d791f3b45214d6648f
Bruno Souza de Lima <bruno.szdl@gmail.com>
2026-04-25 00:05:00 -0300

    fix: fix editor space bug

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -105,6 +105,8 @@ export default function Editor() {
               lineNumbers: 'on',
               renderLineHighlight: 'line',
               padding: { top: 8 },
+              acceptSuggestionOnCommitCharacter: false,
+              wordBasedSuggestions: 'off',
             }}
           />
         )}
